4085ff54c8db6c0c55a71090b783695911c051d0
bsevgeny@gmail.com <papakita2009>
2017-01-16 17:02:24 +0200

    first spring lab solved - write BPP for InjectRandomInt

---
+++ src/main/java/quoters/bpp/InjectRandomIntAnnotationBeanPostProcessor.java
@@ -0,0 +1,38 @@
+package quoters.bpp;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import quoters.InjectRandomInt;
+
+import java.lang.reflect.Field;
+import java.util.Random;
+
+/**
+ * Created by Evegeny on 16/01/2017.
+ */
+public class InjectRandomIntAnnotationBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    @SneakyThrows
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        Class<?> type = bean.getClass();
+        Field[] fields = type.getDeclaredFields();
+        for (Field field : fields) {
+            InjectRandomInt annotation = field.getAnnotation(InjectRandomInt.class);
+            if (annotation != null) {
+                int min = annotation.min();
+                int max = annotation.max();
+                Random random = new Random();
+                field.setAccessible(true);
+                int value = min + random.nextInt(max - min);
+                field.set(bean,value);
+            }
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+}
