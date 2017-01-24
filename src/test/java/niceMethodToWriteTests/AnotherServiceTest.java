b6f69bd98f5e46e98475bfff5005f715161e9643
bsevgeny@gmail.com <papakita2009>
2017-01-24 16:57:23 +0200

    executor service added

--- src/test/java/niceMethodToWriteTests/AnotherServiceTest.java
+++ src/test/java/niceMethodToWriteTests/AnotherServiceTest.java
@@ -4,8 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -21,6 +26,12 @@ public class AnotherServiceTest {
 
     @Test
     public void doStuff() throws Exception {
+
+     /*   ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        executorService.submit(() -> System.out.println("2423"));*/
+
+
 
         MyService myServiceMock = mock(MyService.class);
 
